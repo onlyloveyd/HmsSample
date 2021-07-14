4f6b67803b2a0ff589889ea15f4e1319df5a86eb
onlyloveyd <onlyloveyd@gmail.com>
2021-07-14 21:03:41 +0800

    1.clean code

--- app/src/main/java/tech/kicky/hms/colorfilter/ColorFilterFragment.kt
+++ app/src/main/java/tech/kicky/hms/colorfilter/ColorFilterFragment.kt
@@ -118,43 +118,49 @@ class ColorFilterFragment : Fragment() {
         imageVisionFilterAPI?.stop()
     }
 
-    private fun doFilter() {
-        if (!this::originBitmap.isInitialized) {
-            Toast.makeText(requireContext(), "Please Pick Image", Toast.LENGTH_SHORT).show()
-            return
-        }
-        lifecycleScope.launch(Dispatchers.IO) {
-            val jsonObject = JSONObject()
-            val taskJson = JSONObject()
-            try {
-                taskJson.put("intensity", "1")
-                taskJson.put("filterType", LocalFilters[filterIndex].code)
-                taskJson.put("compressRate", "1")
-                jsonObject.put("requestId", "1")
-                jsonObject.put("taskJson", taskJson)
-                jsonObject.put("authJson", LocalAuthJson)
-                val visionResult = imageVisionFilterAPI?.getColorFilter(
-                    jsonObject,
-                    originBitmap
-                )
-                withContext(Dispatchers.Main) {
-                    if (visionResult?.resultCode == 0) {
-                        val image = visionResult.image
-                        mBinding.picture.setImageBitmap(image)
-                    } else {
-                        Toast.makeText(
-                            requireContext(),
-                            "resultCode: ${visionResult?.resultCode}",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
+private fun doFilter() {
+    if (!this::originBitmap.isInitialized) {
+        Toast.makeText(requireContext(), "Please Pick Image", Toast.LENGTH_SHORT).show()
+        return
+    }
+    lifecycleScope.launch(Dispatchers.IO) {
+        val jsonObject = JSONObject()
+        val taskJson = JSONObject()
+        try {
+            // 滤镜强度，取值范围[0,1.0]，默认为1.0。
+            taskJson.put("intensity", "1")
+            // 颜色映射的图片索引，索引范围[0,24]（0为原图）
+            taskJson.put("filterType", LocalFilters[filterIndex].code)
+            // 压缩率，取值范围（0,1.0]，默认为1.0。
+            taskJson.put("compressRate", "1")
+
+            // 业务提供的请求ID(可选)
+            jsonObject.put("requestId", "1")
+            jsonObject.put("taskJson", taskJson)
+            // 鉴权参数,与初始化过程中使用的相同
+            jsonObject.put("authJson", LocalAuthJson)
+            val visionResult = imageVisionFilterAPI?.getColorFilter(
+                jsonObject,
+                originBitmap
+            )
+            withContext(Dispatchers.Main) {
+                if (visionResult?.resultCode == 0) {
+                    val image = visionResult.image
+                    mBinding.picture.setImageBitmap(image)
+                } else {
+                    Toast.makeText(
+                        requireContext(),
+                        "resultCode: ${visionResult?.resultCode}",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
-                LogsUtil.e(TAG, "visionResult: $visionResult")
-            } catch (e: JSONException) {
-                LogsUtil.e(TAG, "JSONException: " + e.message)
             }
+            LogsUtil.e(TAG, "visionResult: $visionResult")
+        } catch (e: JSONException) {
+            LogsUtil.e(TAG, "JSONException: " + e.message)
         }
     }
+}
 
     private fun pickImage() {
         EasyPhotos.createAlbum(
--- app/src/main/java/tech/kicky/hms/colorfilter/Request.kt
+++ app/src/main/java/tech/kicky/hms/colorfilter/Request.kt
@@ -7,31 +7,6 @@ import org.json.JSONObject
  * author: yidong
  * 2021-07-10
  */
-data class RequestJson(
-    val requestId: String = "",
-    val taskJson: TaskJson,
-    val authJson: AuthJson
-)
-
-data class TaskJson(
-    // 颜色映射的图片索引，索引范围[0,24]（0为原图）
-    val filterType: Int,
-    // 滤镜强度，取值范围[0,1.0]，默认为1.0。
-    val intensity: Float,
-    // 压缩率，取值范围（0,1.0]，默认为1.0。
-    val compressRate: Float
-)
-
-
-data class AuthJson(
-    val projectId: String,
-    val appId: String,
-    val authApiKey: String,
-    val clientSecret: String,
-    val clientId: String,
-)
-
-
 var stringLocal =
     "{\"projectId\":\"736430079245736563\",\"appId\":\"104358187\",\"authApiKey\":\"CgB6e3x9E+mUHoMgSSeRUxNHXL6I2pyf6Ri4BzqWB7eXOtB+EWUqFs1TjDFR911yB8Lw2o/6YOnnXJa93gch8UPo\",\"clientSecret\":\"930975B5741441EAC4D95A6B125EA3445B4E3EEAB247BDD5A4B6B5FFC388B17F\",\"clientId\":\"635146018899374464\",\"token\":\"tokenTest\"}"
 
